695909f18a64137d21ff4bc4fd2215058f1fc533
Gvieve <gnuebel84@yahoo.com>
2021-02-22 10:08:01 -0700

    Create mechanics_controller, action and index view

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper'
 
 describe 'When I visit a mechanics index page' do
   before :each do
-    avery = Mechanic.create!(name: "Avery Jones", years_experience: 8)
-    herbert = Mechanic.create!(name: "Herbert Fallon", years_experience: 3)
-    elon = Mechanic.create!(name: "Elon Musk", years_experience: 11)
-    sarah = Mechanic.create!(name: "Sarah Ledbetter", years_experience: 5)
+    @avery = Mechanic.create!(name: "Avery Jones", years_experience: 8)
+    @herbert = Mechanic.create!(name: "Herbert Fallon", years_experience: 3)
+    @elon = Mechanic.create!(name: "Elon Musk", years_experience: 11)
+    @sarah = Mechanic.create!(name: "Sarah Ledbetter", years_experience: 5)
   end
 
   it 'I see a list of all mechanics names and years of experience' do
     visit mechanics_path
 
     expect(page).to have_content("All Mechanics")
-    expect(page).to have_content("Avery Jones: 8 years of experience")
-    expect(page).to have_content("Herbert Fallon: 3 years of experience")
-    expect(page).to have_content("Elon Musk: 11 years of experience")
-    expect(page).to have_content("Sarah Ledbetter: 5 years of experience")
+
+    within ".mechanics" do
+      expect(page).to have_content("Avery Jones: 8 years of experience")
+      expect(page).to have_content("Herbert Fallon: 3 years of experience")
+      expect(page).to have_content("Elon Musk: 11 years of experience")
+      expect(page).to have_content("Sarah Ledbetter: 5 years of experience")
+    end
   end
 end
