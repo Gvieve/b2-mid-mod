5e94cb320b1e151f86476e5cccae5c144e353154
Gvieve <gnuebel84@yahoo.com>
2021-02-22 09:42:18 -0700

    Create model validation tests for mechnic and ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,6 @@
 class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  validates_presence_of :name
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,8 @@ describe Ride, model: :type do
     it {should have_many :mechanic_rides}
     it {should have_many(:mechanics).through(:mechanic_rides)}
   end
+
+  describe 'validations' do
+    it {should validate_presence_of :name}
+  end
 end
