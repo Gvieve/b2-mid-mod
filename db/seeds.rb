a006b0e4d39bb69e4d893de22b89a7d0b3ddc2e7
Gvieve <gnuebel84@yahoo.com>
2021-02-22 10:07:26 -0700

    Add mechanics to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Mechanic.destroy_all
+
+avery = Mechanic.create!(name: "Avery Jones", years_experience: 8)
+herbert = Mechanic.create!(name: "Herbert Fallon", years_experience: 3)
+elon = Mechanic.create!(name: "Elon Musk", years_experience: 11)
+sarah = Mechanic.create!(name: "Sarah Ledbetter", years_experience: 5)
