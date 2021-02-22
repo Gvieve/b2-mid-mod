9880264c34f33974cf65fbd3a5d3b603643184fb
Gvieve <gnuebel84@yahoo.com>
2021-02-22 11:58:46 -0700

    Create route for mechanic_rides and add form_with to show page

---
+++ app/controllers/mechanic_rides_controller.rb
@@ -0,0 +1,5 @@
+class MechanicRidesController < ApplicationController
+  def create
+    require "pry"; binding.pry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :mechanics
+
+  post "/mechanics/mechanic_rides", to: "mechanic_rides#create", as: :mechanic_rides_create
 end
